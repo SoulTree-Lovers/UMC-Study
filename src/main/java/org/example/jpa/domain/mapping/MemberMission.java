e1b94bf8d5eb27f367d438209b0040014ebdbed8
kangseungmin <rkdtmdals999@naver.com>
2024-05-21 22:43:46 +0900

    feat: Member - Mission 매핑 테이블

---
+++ src/main/java/org/example/jpa/domain/mapping/MemberMission.java
@@ -0,0 +1,32 @@
+package org.example.jpa.domain.mapping;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.jpa.common.BaseEntity;
+import org.example.jpa.domain.member.repository.entity.Member;
+import org.example.jpa.domain.mission.enums.MissionStatus;
+import org.example.jpa.domain.mission.repository.entity.Mission;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberMission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private MissionStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id")
+    private Mission mission;
+
+}
