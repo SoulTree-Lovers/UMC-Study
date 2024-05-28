6d52f2f94a98e98874bd8f5295f70c1d5496911d
kangseungmin <rkdtmdals999@naver.com>
2024-05-28 09:22:50 +0900

    feat: BaseEntity 상속 및 Store와 연관관계 설정

--- src/main/java/org/example/jpa/domain/region/repository/entity/Region.java
+++ src/main/java/org/example/jpa/domain/region/repository/entity/Region.java
@@ -1,22 +1,27 @@
 package org.example.jpa.domain.region.repository.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import org.example.jpa.common.BaseEntity;
+import org.example.jpa.domain.store.repository.entity.Store;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Region {
+public class Region extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 20)
     private String name;
 
+    @OneToMany(mappedBy = "region", fetch = FetchType.LAZY)
+    private List<Store> storeList = new ArrayList<>();
 }
