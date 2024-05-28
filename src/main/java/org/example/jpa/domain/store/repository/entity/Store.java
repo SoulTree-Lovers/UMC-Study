f9a7a8e5954eaa00c4f9c46ee89e38bc9e2060b0
kangseungmin <rkdtmdals999@naver.com>
2024-05-28 19:16:01 +0900

    feat: BaseEntity 상속 및 연관관계 설정

--- src/main/java/org/example/jpa/domain/store/repository/entity/Store.java
+++ src/main/java/org/example/jpa/domain/store/repository/entity/Store.java
@@ -1,26 +1,41 @@
 package org.example.jpa.domain.store.repository.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import org.example.jpa.common.BaseEntity;
+import org.example.jpa.domain.mission.repository.entity.Mission;
+import org.example.jpa.domain.region.repository.entity.Region;
+import org.example.jpa.domain.review.repository.entity.Review;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Store {
+public class Store extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 50, nullable = false)
     private String name;
 
+    @Column(length = 50)
     private String address;
 
     private Float score;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Review> reviewList = new ArrayList<>();
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Mission> missionList = new ArrayList<>();
 }
