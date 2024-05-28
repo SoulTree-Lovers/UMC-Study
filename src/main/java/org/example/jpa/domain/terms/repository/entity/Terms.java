db5e51731483c285479d260ca70b1735180c9036
kangseungmin <rkdtmdals999@naver.com>
2024-05-28 19:16:37 +0900

    feat: BaseEntity 상속 및 title 제약조건 설정

--- src/main/java/org/example/jpa/domain/terms/repository/entity/Terms.java
+++ src/main/java/org/example/jpa/domain/terms/repository/entity/Terms.java
@@ -2,6 +2,7 @@ package org.example.jpa.domain.terms.repository.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.example.jpa.common.BaseEntity;
 import org.example.jpa.domain.mapping.MemberAgree;
 
 import java.util.ArrayList;
@@ -12,12 +13,13 @@ import java.util.List;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Terms {
+public class Terms extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 20, nullable = false)
     private String title;
 
     private String body;
