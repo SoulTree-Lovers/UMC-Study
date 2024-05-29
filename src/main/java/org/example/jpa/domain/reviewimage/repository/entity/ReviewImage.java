b8aedff7635b22161bfcc5fdc3b3986fb0cdb249
kangseungmin <rkdtmdals999@naver.com>
2024-05-29 22:28:37 +0900

    feat: ReviewImage Entity 추가

---
+++ src/main/java/org/example/jpa/domain/reviewimage/repository/entity/ReviewImage.java
@@ -0,0 +1,28 @@
+package org.example.jpa.domain.reviewimage.repository.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import lombok.*;
+import org.example.jpa.common.BaseEntity;
+import org.example.jpa.domain.review.repository.entity.Review;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ReviewImage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+}
