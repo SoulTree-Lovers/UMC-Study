69e2d1a79e315ba09a0bcd85f154596d5e2f06a3
kangseungmin <rkdtmdals999@naver.com>
2024-05-29 22:28:23 +0900

    feat: BaseEntity 상속 및 속성, 연관관계 추가

--- src/main/java/org/example/jpa/domain/review/repository/entity/Review.java
+++ src/main/java/org/example/jpa/domain/review/repository/entity/Review.java
@@ -2,23 +2,39 @@ package org.example.jpa.domain.review.repository.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.example.jpa.common.BaseEntity;
 import org.example.jpa.domain.member.repository.entity.Member;
+import org.example.jpa.domain.reviewimage.repository.entity.ReviewImage;
+import org.example.jpa.domain.store.repository.entity.Store;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Review {
+//@DependsOn(value = "member")
+public class Review extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String title;
+    private String body;
 
     private Float score;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
     private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<ReviewImage> reviewImageList = new ArrayList<>();
+
 }
