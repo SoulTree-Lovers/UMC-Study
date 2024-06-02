a1e8cfce433036a98264b93f5750e9289ca464f3
kangseungmin <rkdtmdals999@naver.com>
2024-06-02 21:44:08 +0900

    feat: 베이스 코드 인터페이스

---
+++ src/main/java/org/example/jpa/api/code/BaseCode.java
@@ -0,0 +1,8 @@
+package org.example.jpa.api.code;
+
+public interface BaseCode {
+
+    public Reason getReason();
+
+    public Reason getReasonHttpStatus();
+}
---
+++ src/main/java/org/example/jpa/api/code/BaseErrorCode.java
@@ -0,0 +1,9 @@
+package org.example.jpa.api.code;
+
+public interface BaseErrorCode {
+
+    public ErrorReason getReason();
+
+    public ErrorReason getReasonHttpStatus();
+
+}
