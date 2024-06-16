0d2f106244ac45ad3ad31e16223a18cc3d57b789
kangseungmin <rkdtmdals999@naver.com>
2024-06-17 00:10:16 +0900

    refactor: Valid 속성 추가

--- src/main/java/org/example/jpa/domain/member/controller/MemberRequestDto.java
+++ src/main/java/org/example/jpa/domain/member/controller/MemberRequestDto.java
@@ -1,6 +1,10 @@
 package org.example.jpa.domain.member.controller;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import org.example.jpa.validation.annotation.ExistCategories;
 
 import java.util.List;
 
@@ -8,13 +12,21 @@ public class MemberRequestDto {
 
     @Getter
     public static class JoinDto{
+        @NotBlank
         String name;
+        @NotNull
         Integer gender;
+        @NotNull
         Integer birthYear;
+        @NotNull
         Integer birthMonth;
+        @NotNull
         Integer birthDay;
+        @Size(min = 5, max = 12)
         String address;
+        @Size(min = 5, max = 12)
         String specAddress;
+        @ExistCategories
         List<Long> preferCategory;
     }
 
