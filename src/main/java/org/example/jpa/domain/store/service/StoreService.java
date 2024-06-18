580c0f19e7fd0c8aee75d27c3f2c0caff36fc17b
kangseungmin <rkdtmdals999@naver.com>
2024-06-19 00:47:12 +0900

    feat: findStore() 메소드 추가

--- src/main/java/org/example/jpa/domain/store/service/StoreService.java
+++ src/main/java/org/example/jpa/domain/store/service/StoreService.java
@@ -1,16 +1,15 @@
 package org.example.jpa.domain.store.service;
 
 import jakarta.validation.ConstraintValidatorContext;
-import org.example.jpa.domain.mission.controller.MissionChangeRequestDto;
-import org.example.jpa.domain.store.controller.StoreMissionRequestDto;
-import org.example.jpa.domain.store.controller.StoreReviewRequestDto;
+import org.example.jpa.domain.store.controller.dto.StoreMissionRequestDto;
+import org.example.jpa.domain.store.controller.dto.StoreReviewRequestDto;
 import org.example.jpa.domain.store.repository.entity.Store;
-
-import java.util.List;
 
 public interface StoreService {
 
     Store addReview(StoreReviewRequestDto storeReviewRequestDto);
     Store addMission(StoreMissionRequestDto storeMissionRequestDto);
+
+    Store findStore(Long id);
     boolean isValid(Long storeId, ConstraintValidatorContext context);
 }
