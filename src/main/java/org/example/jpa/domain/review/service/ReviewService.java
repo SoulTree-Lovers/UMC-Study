f07e48d3bab4ff52bf6a1a9c0fe3c4879af5f55b
kangseungmin <rkdtmdals999@naver.com>
2024-06-20 02:34:55 +0800

    feat: ReviewService 생성

---
+++ src/main/java/org/example/jpa/domain/review/service/ReviewService.java
@@ -0,0 +1,11 @@
+package org.example.jpa.domain.review.service;
+
+import org.example.jpa.domain.review.repository.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+public interface ReviewService {
+
+    Page<Review> getReviewList(Long storeId, Integer page);
+
+}
