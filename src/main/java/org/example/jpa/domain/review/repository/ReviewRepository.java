91bfeb2c410439e74e0aa5c05199577683086f82
kangseungmin <rkdtmdals999@naver.com>
2024-06-20 02:23:22 +0800

    feat: findAllByStore 메소드 추가

--- src/main/java/org/example/jpa/domain/review/repository/ReviewRepository.java
+++ src/main/java/org/example/jpa/domain/review/repository/ReviewRepository.java
@@ -1,7 +1,13 @@
 package org.example.jpa.domain.review.repository;
 
 import org.example.jpa.domain.review.repository.entity.Review;
+import org.example.jpa.domain.store.repository.entity.Store;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    Page<Review> findAllByStore(Store store, PageRequest pageRequest);
+
 }
