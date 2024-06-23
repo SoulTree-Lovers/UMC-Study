2ec0dcd8cd478a2d38440360fa0121b97bce09d6
kangseungmin <rkdtmdals999@naver.com>
2024-06-23 20:32:46 +0900

    feat: 특정 가게의 미션 목록 조회 repository 인터페이스 구현

--- src/main/java/org/example/jpa/domain/mission/repository/MissionRepository.java
+++ src/main/java/org/example/jpa/domain/mission/repository/MissionRepository.java
@@ -1,7 +1,14 @@
 package org.example.jpa.domain.mission.repository;
 
 import org.example.jpa.domain.mission.repository.entity.Mission;
+import org.example.jpa.domain.store.repository.entity.Store;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MissionRepository extends JpaRepository<Mission, Long> {
+
+    Page<Mission> findAllByStore(Store store, PageRequest pageRequest);
 }
