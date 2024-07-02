dbb42cf5b549fdff312465ffd784de8f9e48ac8d
kangseungmin <rkdtmdals999@naver.com>
2024-07-03 06:58:15 +0900

    feat: OAuth 인증 로직 메소드 서비스 인터페이스 추가

--- src/main/java/org/example/jpa/domain/member/service/MemberService.java
+++ src/main/java/org/example/jpa/domain/member/service/MemberService.java
@@ -1,11 +1,15 @@
 package org.example.jpa.domain.member.service;
 
 import org.example.jpa.domain.member.controller.MemberRequestDto;
+import org.example.jpa.domain.member.enums.SocialType;
 import org.example.jpa.domain.member.repository.entity.Member;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.stereotype.Service;
 
 public interface MemberService {
 
 
     Member joinMember(MemberRequestDto.JoinDto request);
+
+    void processOAuthUser(String email, SocialType socialType);
 }
